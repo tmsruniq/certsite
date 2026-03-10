25d4ac32db2625345313eed583e3871928b2e4bf
SRUNIQ <admin@sruniq.com>
2026-03-10 21:44:04 +0530

    remove search system and use user certificate view

--- certificates/urls.py
+++ certificates/urls.py
@@ -2,13 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from certificates import views
 
-from django.conf import settings
-from django.conf.urls.static import static
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.my_certificates, name='my_certificates'),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

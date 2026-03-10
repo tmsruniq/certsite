8ffba5e8631f121d6d3ffb94d49bf2b6d0af335d
SRUNIQ <admin@sruniq.com>
2026-03-10 20:10:26 +0530

    add login template

--- certsite/settings.py
+++ certsite/settings.py
@@ -54,19 +54,22 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'certsite.urls'
 
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
+{
+    'BACKEND': 'django.template.backends.django.DjangoTemplates',
+    'DIRS': [BASE_DIR / "templates"],   # 👈 add this
+    'APP_DIRS': True,
+    'OPTIONS': {
+        'context_processors': [
+            'django.template.context_processors.request',
+            'django.contrib.auth.context_processors.auth',
+            'django.contrib.messages.context_processors.messages',
+        ],
     },
+}
 ]
 
 WSGI_APPLICATION = 'certsite.wsgi.application'

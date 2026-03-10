d741e10915fb964cd46f073d65aeea8647d51686
SRUNIQ <admin@sruniq.com>
2026-03-10 21:51:17 +0530

    fix view name

--- certificates/urls.py
+++ certificates/urls.py
@@ -4,6 +4,9 @@ from certificates import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # show logged in user's certificates
     path('', views.my_certificates, name='my_certificates'),
+
     path('accounts/', include('django.contrib.auth.urls')),
 ]

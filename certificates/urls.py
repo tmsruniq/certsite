ec7973b5f2357175514a31f3ea4879e6921c5138
SRUNIQ <admin@sruniq.com>
2026-03-10 13:36:15 +0530

    Add django auth urls for login

--- certificates/urls.py
+++ certificates/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from certificates import views
 
 from django.conf import settings
@@ -7,10 +7,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    # homepage search
     path('', views.search_certificate, name='search_certificate'),
+    path('accounts/', include('django.contrib.auth.urls')),  # login/logout URLs
 ]
 
-# serve media files (certificate PDFs)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

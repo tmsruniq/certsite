dfc4ef567ef7d62d4cc1187cb1e540ca145bc89b
SRUNIQ <admin@sruniq.com>
2026-03-10 11:01:09 +0530

    fix urls for media

--- certificates/urls.py
+++ certificates/urls.py
@@ -1,6 +1,16 @@
+from django.contrib import admin
 from django.urls import path
-from . import views
+from certificates import views
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('search/', views.search_certificate, name='search'),
+    path('admin/', admin.site.urls),
+
+    # homepage search
+    path('', views.search_certificate, name='search_certificate'),
 ]
+
+# serve media files (certificate PDFs)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- certificates/views.py
+++ certificates/views.py
@@ -2,15 +2,19 @@ from django.shortcuts import render
 from .models import Certificate
 
 def search_certificate(request):
+
     cert = None
 
     if request.method == "POST":
-        cert_no = request.POST.get("certificate_number")
-        serial = request.POST.get("instrument_serial")
+        certificate_number = request.POST.get("certificate_number")
+        instrument_serial = request.POST.get("instrument_serial")
 
-        cert = Certificate.objects.filter(
-            certificate_number=cert_no,
-            instrument_serial=serial
-        ).first()
+        try:
+            cert = Certificate.objects.get(
+                certificate_number=certificate_number,
+                instrument_serial=instrument_serial
+            )
+        except Certificate.DoesNotExist:
+            cert = None
 
     return render(request, "search.html", {"cert": cert})

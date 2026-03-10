5d8079026417e15317dba4b41c5ec7e55103d0d0
SRUNIQ <admin@sruniq.com>
2026-03-10 21:24:03 +0530

    show certificates after login

--- certificates/views.py
+++ certificates/views.py
@@ -3,21 +3,8 @@ from django.contrib.auth.decorators import login_required
 from .models import Certificate
 
 @login_required
-def search_certificate(request):
-    cert = None
+def my_certificates(request):
+    # get certificates only for logged-in user
+    certs = Certificate.objects.filter(user=request.user)
 
-    if request.method == "POST":
-        certificate_number = request.POST.get("certificate_number")
-        instrument_serial = request.POST.get("instrument_serial")
-        user = request.user  # only fetch certificates for this user
-
-        try:
-            cert = Certificate.objects.get(
-                certificate_number=certificate_number,
-                instrument=instrument_serial,
-                user=user  # ✅ restrict to logged-in user
-            )
-        except Certificate.DoesNotExist:
-            cert = None
-
-    return render(request, "search.html", {"cert": cert})
+    return render(request, "certificates.html", {"certs": certs})
--- certsite/urls.py
+++ certsite/urls.py
@@ -4,6 +4,6 @@ from certificates import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.search_certificate, name='search_certificate'),
+    path('', views.search_certificate, name='my_certificates'),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
